98da9d0563f646c669006feebc701737a1a3e09e
Sunghyeon Cho <chosh040@naver.com>
2024-02-01 21:38:56 +0900

    Title: 오목, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240201/오목/O-mok.cpp
+++ 240201/오목/O-mok.cpp
@@ -5,10 +5,18 @@ int g[20][20] = {0};
 
 
 bool check(int x, int y) {
-    if(g[x][y] == g[x - 2][y] && g[x][y] == g[x - 1][y] && g[x][y] == g[x + 1][y] && g[x][y] == g[x + 2][y]) return true;
-    if(g[x][y] == g[x - 2][y + 2] && g[x][y] == g[x - 1][y + 1] && g[x][y] == g[x + 1][y- 1] && g[x][y] == g[x + 2][y-2]) return true;
-    if(g[x][y] == g[x][y + 2] && g[x][y] == g[x][y + 1] && g[x][y] == g[x][y-1] && g[x][y] == g[x][y-2]) return true;
-    if(g[x][y] == g[x + 2][y + 2] && g[x][y] == g[x + 1][y + 1] && g[x][y] == g[x -2][y-2] && g[x][y] == g[x -1][y-1]) return true;
+    if(x >= 2 && x <= 17) {
+        
+        if(y >=2 && y <=17) {
+            if(g[x][y] == g[x - 2][y + 2] && g[x][y] == g[x - 1][y + 1] && g[x][y] == g[x + 1][y- 1] && g[x][y] == g[x + 2][y-2]) return true;
+            if(g[x][y] == g[x + 2][y + 2] && g[x][y] == g[x + 1][y + 1] && g[x][y] == g[x -2][y-2] && g[x][y] == g[x -1][y-1]) return true;
+        }
+    } 
+    
+    if(x >= 2 && x <= 17 && g[x][y] == g[x - 2][y] && g[x][y] == g[x - 1][y] && g[x][y] == g[x + 1][y] && g[x][y] == g[x + 2][y]) return true;
+    
+    if(y >=2 && y <=17 && g[x][y] == g[x][y + 2] && g[x][y] == g[x][y + 1] && g[x][y] == g[x][y-1] && g[x][y] == g[x][y-2]) return true;
+    
     
 
     return false;
@@ -19,8 +27,8 @@ int main() {
         for(int j =0; j < 19; j++) cin>>g[i][j];
     }
 
-    for(int i =2; i < 17; i++) {
-        for(int j =2; j< 17; j++) {
+    for(int i =0; i < 19; i++) {
+        for(int j =0; j< 19; j++) {
             if(g[i][j] != 0) {
                 if(check(i, j)) {
                     cout<< g[i][j] << '\n' << i + 1<< ' '<<j +1;
