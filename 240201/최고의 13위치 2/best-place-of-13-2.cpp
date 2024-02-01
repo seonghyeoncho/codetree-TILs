357fcf7ca3a37bc6bcbefa345547a64ba7d394b6
Sunghyeon Cho <chosh040@naver.com>
2024-02-01 22:49:50 +0900

    Title: 최고의 13위치 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240201/최고의 13위치 2/best-place-of-13-2.cpp
+++ 240201/최고의 13위치 2/best-place-of-13-2.cpp
@@ -15,13 +15,15 @@ int main() {
 
     for(int i = 0 ; i < n; i++) {
         for(int j = 0; j < n- 2; j++) {
-            int sum = g[i][j] + g[i][j + 1] + g[i][j + 2];
+            
 
             for(int k = 0 ; k < n; k++) {
                 for(int l = 0; l < n-2; l++) {
+                    int sum = g[i][j] + g[i][j + 1] + g[i][j + 2];
+                    
                     if(k == i && (l == j || l == j + 1 || l == j + 2)) continue;
                     else sum += g[k][l] + g[k][l + 1] + g[k][l + 2];
-
+                    
                     if(sum > answer) answer = sum;
                 }
             }
