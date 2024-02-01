e17e7c9ad018145c8bd38acda6e5c5688e11bf6a
Sunghyeon Cho <chosh040@naver.com>
2024-02-01 22:48:05 +0900

    Title: 최고의 13위치 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240201/최고의 13위치 2/best-place-of-13-2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+int n, answer = 0;
+int g[21][21] = {0};
+
+int main() {
+    cin>>n;
+
+    for(int i = 0 ; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cin>>g[i][j];
+        }
+    }
+
+    for(int i = 0 ; i < n; i++) {
+        for(int j = 0; j < n- 2; j++) {
+            int sum = g[i][j] + g[i][j + 1] + g[i][j + 2];
+
+            for(int k = 0 ; k < n; k++) {
+                for(int l = 0; l < n-2; l++) {
+                    if(k == i && (l == j || l == j + 1 || l == j + 2)) continue;
+                    else sum += g[k][l] + g[k][l + 1] + g[k][l + 2];
+
+                    if(sum > answer) answer = sum;
+                }
+            }
+
+            
+        }
+    }
+    
+    cout<<answer;
+
+    return 0;
+}
